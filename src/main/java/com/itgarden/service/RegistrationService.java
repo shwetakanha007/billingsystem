12834665a5fe16936ecd8831c2fda133c083ea44
shwetakanha007 <124570132+shwetakanha007@users.noreply.github.com>
2025-08-29 13:23:09 -0400

    Update RegistrationService.java

--- src/main/java/com/itgarden/service/RegistrationService.java
+++ src/main/java/com/itgarden/service/RegistrationService.java
@@ -49,6 +49,7 @@ public class RegistrationService {
             throw new InvalidInputException(String.format("Invalid user type %s ", type));
         } catch (Exception e) {
             e.printStackTrace();
+            e.message();
             throw e;
         }
         return responseMessage;
